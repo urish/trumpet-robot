adb3e7c4c8016561be9663da329e4b2db55304d0
Uri Shaked <uri@urishaked.com>
2018-10-22 23:32:46 +0300

    feat(firmware): add a sequence for fingers

--- firmware/fingers.js
+++ firmware/fingers.js
@@ -1,7 +1,9 @@
 // Trumpet Playing Robot: Finger Controller
 // Copyright (C) 2018, Uri Shaked. License: MIT
 
-class ServoButton {
+let buttonsEnabled = true;
+
+class FingerController {
   constructor(options) {
     this.pin = options.pin;
     this.button = options.button;
@@ -12,7 +14,9 @@ class ServoButton {
   }
 
   update() {
-    this.setState(digitalRead(this.button));
+    if (buttonsEnabled) {
+      this.setState(digitalRead(this.button));
+    }
     if (this.state) {
       digitalPulse(this.pin, HIGH, this.down);
       this.upCounter = 0;
@@ -29,19 +33,58 @@ class ServoButton {
   }
 }
 
-const buttons = [
-  new ServoButton({ pin: D8, button: BTN1, up: 2.1, down: 0.9 }),
-  new ServoButton({ pin: D9, button: BTN2, up: 2.2, down: 1.1 }),
-  new ServoButton({ pin: D10, button: BTN3, up: 0.9, down: 1.9 }),
+const fingers = [
+  new FingerController({ pin: D8, button: BTN1, up: 2.1, down: 0.9 }),
+  new FingerController({ pin: D9, button: BTN2, up: 2.2, down: 1.1 }),
+  new FingerController({ pin: D10, button: BTN3, up: 0.9, down: 1.9 }),
 ];
 
-// Servo
 function updateServos() {
-  for (let i = 0; i < buttons.length; i++) {
-    buttons[i].update();
+  for (let i = 0; i < fingers.length; i++) {
+    fingers[i].update();
   }
+}
+
+const sequence = 'L2 E F G F E D L4 E D E F E'.split(' ');
+const noteMap = {
+  D: [0, 2],
+  E: [0],
+  F: [2],
+  G: [],
+};
+function playSequence() {
+  if (!buttonsEnabled) {
+    return;
+  }
+
+  buttonsEnabled = false;
+  let wholeLength = 800;
+  let noteLength = wholeLength / 4;
+  let time = 0;
+  for (const note of sequence) {
+    if (note[0] === 'L') {
+      noteLength = wholeLength / parseInt(note.substr(1));
+    }
+    if (noteMap[note]) {
+      const noteFingers = noteMap[note];
+      setTimeout(
+        function(noteFingers) {
+          for (let i = 0; i < fingers.length; i++) {
+            fingers[i].state = noteFingers.indexOf(i) >= 0;
+          }
+          updateServos();
+        }.bind(this, noteFingers),
+        time,
+      );
+      time += noteLength;
+    }
+  }
+  setTimeout(() => {
+    buttonsEnabled = true;
+  }, time);
 }
 
 function onInit() {
   setInterval(updateServos, 20);
+  setWatch(playSequence, BTN4, { repeat: true, edge: 'rising', debounce: 25 });
 }
